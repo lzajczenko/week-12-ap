0c16349713ee1288899843a0883d67d3e3e592fd
lzajczenko <lzajczenko@cps.edu>
2025-11-13 18:50:03 +0000

    Week_13

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -41,9 +41,16 @@ print(b == b) #True
 # Create a simple grade-checking condition:
 
 # practice problem :
-# where a student must check if their score is greater than or equal to 60 to pass a test.# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
-grade = int(input("Enter your name"))
+# where a student must check if their score is greater than or equal to 60 to pass a test.
+grade = int(input("Enter your grade"))
 if grade >= 60:
     print("You passed the test!")
 else:
     print("You did not pass the test.")
+
+# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
+password = input("Enter your password: ")
+if len(password) >= 8 and any (char.isdigit() for char in password):
+    print("Password is valid.")
+else:
+    print("Password is invalid.")
